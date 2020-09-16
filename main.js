b9f7acf40ac59ccd166bcf2697ace7ae847aa815
github_wolebabatunde <kolawole_yekini@yahoo.com>
2020-09-16 14:00:17 +0100

    Javascript Jumbotron Slider

---
+++ main.js
@@ -0,0 +1,91 @@
+
+function marg1(){
+    var slider=document.querySelector('#slider');
+    slider.style.marginLeft='-100%';
+    function marg2(){
+        var slider=document.querySelector('#slider');
+        slider.style.marginLeft='-200%';
+        function marg0(){
+            var slider=document.querySelector('#slider');
+            slider.style.marginLeft='0'
+            setTimeout(marg1, 8000);
+        }
+        setTimeout(marg0, 8000);
+    }
+    setTimeout(marg2, 8000);
+}
+
+setInterval(slid, 7500);
+setInterval(para, 7200);
+setInterval(head, 7200);
+setInterval(line, 7500);
+setTimeout(marg1, 8000);
+setInterval(slid2, 8500);
+setInterval(head2, 8700);
+setInterval(para2, 8700);
+setInterval(line2, 8600);
+
+function slid(){
+    var s=document.querySelectorAll('.slid');
+    var i;
+    for (i=0; i<s.length; i++){
+        s[i].style.transfrom='scale(.8)';
+    }
+}
+
+function slid2(){
+    var s=document.querySelectorAll('.slid');
+    var i;
+    for (i=0; i<s.length; i++){
+        s[i].style.transfrom='scale(1)';
+    }
+}
+
+function head(){
+    var h=document.querySelectorAll('h1');
+    var i;
+    for (i=0; i<h.length; i++){
+        h[i].style.top='70px';
+    }
+}
+
+function head2(){
+    var h=document.querySelectorAll('h1');
+    var i;
+    for (i=0; i<h.length; i++){
+        h[i].style.top='0';
+    }
+}
+
+function para(){
+    var p=document.querySelectorAll('p');
+    var i; 
+    for (i=0; i<p.length; i++){
+        p[i].style.bottom='40px';
+    }
+}
+
+function para2(){
+    var p=document.querySelectorAll('p');
+    var i; 
+    for (i=0; i<p.length; i++){
+        p[i].style.bottom='0';
+    }
+}
+
+function line(){
+    var l=document.querySelectorAll('.line');
+    var i;
+    for (i=0; i<l.length; i++){
+        l[i].style.transfrom='scale(0)';
+    }
+}
+
+function line2(){
+    var l=document.querySelectorAll('.line');
+    var i;
+    for (i=0; i<l.length; i++){
+        l[i].style.transfrom='scale(1)';
+    }
+
+}
